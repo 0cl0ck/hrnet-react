faead583a75d3325fe46fb8d9791a04b4ac01aef
Hugo Dewas <hugodewas@gmail.com>
2024-06-20 01:42:00 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { Link } from "react-router-dom";
+import "react-datepicker/dist/react-datepicker.css";
+
+const Home = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    department: "",
+  });
+
+  const states = [
+    { name: "Alabama", abbreviation: "AL" },
+    { name: "Alaska", abbreviation: "AK" },
+    { name: "American Samoa", abbreviation: "AS" },
+    { name: "Arizona", abbreviation: "AZ" },
+    { name: "Arkansas", abbreviation: "AR" },
+    { name: "California", abbreviation: "CA" },
+    { name: "Colorado", abbreviation: "CO" },
+    { name: "Connecticut", abbreviation: "CT" },
+    { name: "Delaware", abbreviation: "DE" },
+    { name: "District Of Columbia", abbreviation: "DC" },
+    { name: "Federated States Of Micronesia", abbreviation: "FM" },
+    { name: "Florida", abbreviation: "FL" },
+    { name: "Georgia", abbreviation: "GA" },
+    { name: "Guam", abbreviation: "GU" },
+    { name: "Hawaii", abbreviation: "HI" },
+    { name: "Idaho", abbreviation: "ID" },
+    { name: "Illinois", abbreviation: "IL" },
+    { name: "Indiana", abbreviation: "IN" },
+    { name: "Iowa", abbreviation: "IA" },
+    { name: "Kansas", abbreviation: "KS" },
+    { name: "Kentucky", abbreviation: "KY" },
+    { name: "Louisiana", abbreviation: "LA" },
+    { name: "Maine", abbreviation: "ME" },
+    { name: "Marshall Islands", abbreviation: "MH" },
+    { name: "Maryland", abbreviation: "MD" },
+    { name: "Massachusetts", abbreviation: "MA" },
+    { name: "Michigan", abbreviation: "MI" },
+    { name: "Minnesota", abbreviation: "MN" },
+    { name: "Mississippi", abbreviation: "MS" },
+    { name: "Missouri", abbreviation: "MO" },
+    { name: "Montana", abbreviation: "MT" },
+    { name: "Nebraska", abbreviation: "NE" },
+    { name: "Nevada", abbreviation: "NV" },
+    { name: "New Hampshire", abbreviation: "NH" },
+    { name: "New Jersey", abbreviation: "NJ" },
+    { name: "New Mexico", abbreviation: "NM" },
+    { name: "New York", abbreviation: "NY" },
+    { name: "North Carolina", abbreviation: "NC" },
+    { name: "North Dakota", abbreviation: "ND" },
+    { name: "Northern Mariana Islands", abbreviation: "MP" },
+    { name: "Ohio", abbreviation: "OH" },
+    { name: "Oklahoma", abbreviation: "OK" },
+    { name: "Oregon", abbreviation: "OR" },
+    { name: "Palau", abbreviation: "PW" },
+    { name: "Pennsylvania", abbreviation: "PA" },
+    { name: "Puerto Rico", abbreviation: "PR" },
+    { name: "Rhode Island", abbreviation: "RI" },
+    { name: "South Carolina", abbreviation: "SC" },
+    { name: "South Dakota", abbreviation: "SD" },
+    { name: "Tennessee", abbreviation: "TN" },
+    { name: "Texas", abbreviation: "TX" },
+    { name: "Utah", abbreviation: "UT" },
+    { name: "Vermont", abbreviation: "VT" },
+    { name: "Virgin Islands", abbreviation: "VI" },
+    { name: "Virginia", abbreviation: "VA" },
+    { name: "Washington", abbreviation: "WA" },
+    { name: "West Virginia", abbreviation: "WV" },
+    { name: "Wisconsin", abbreviation: "WI" },
+    { name: "Wyoming", abbreviation: "WY" },
+  ];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const saveEmployee = () => {
+    const employees = JSON.parse(localStorage.getItem("employees")) || [];
+    employees.push({ ...formState, startDate, birthDate });
+    localStorage.setItem("employees", JSON.stringify(employees));
+    alert("Employee Created!");
+  };
+
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>HRnet</h1>
+      </div>
+      <Link to="/employee-list">View Current Employees</Link>
+      <h2>Create Employee</h2>
+      <form
+        id="create-employee"
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveEmployee();
+        }}
+      >
+        <label htmlFor="first-name">First Name</label>
+        <input
+          type="text"
+          id="first-name"
+          name="firstName"
+          value={formState.firstName}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          type="text"
+          id="last-name"
+          name="lastName"
+          value={formState.lastName}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="date-of-birth">Date of Birth</label>
+        <DatePicker
+          selected={birthDate}
+          onChange={(date) => setBirthDate(date)}
+        />
+
+        <label htmlFor="start-date">Start Date</label>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+
+        <fieldset className="address">
+          <legend>Address</legend>
+
+          <label htmlFor="street">Street</label>
+          <input
+            type="text"
+            id="street"
+            name="street"
+            value={formState.street}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={formState.city}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="state">State</label>
+          <select
+            name="state"
+            id="state"
+            value={formState.state}
+            onChange={handleChange}
+          >
+            {states.map((state) => (
+              <option key={state.abbreviation} value={state.abbreviation}>
+                {state.name}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="zip-code">Zip Code</label>
+          <input
+            type="number"
+            id="zip-code"
+            name="zipCode"
+            value={formState.zipCode}
+            onChange={handleChange}
+          />
+        </fieldset>
+
+        <label htmlFor="department">Department</label>
+        <select
+          name="department"
+          id="department"
+          value={formState.department}
+          onChange={handleChange}
+        >
+          <option>Sales</option>
+          <option>Marketing</option>
+          <option>Engineering</option>
+          <option>Human Resources</option>
+          <option>Legal</option>
+        </select>
+        <button type="submit">Save</button>
+      </form>
+      {/* <div id="confirmation" className="modal">
+        Employee Created!
+      </div> */}
+    </div>
+  );
+};
+
+export default Home;
