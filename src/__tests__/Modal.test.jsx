3c9607c6283a2634031840b22d6a3b719c467eaa
Hugo Dewas <hugodewas@gmail.com>
2024-08-17 15:55:40 +0200

    correction test Modal.test.jsx import modal

--- src/__tests__/Modal.test.jsx
+++ src/__tests__/Modal.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import Modal from "../react-modal-component/src/Modal";
+import Modal from "../components/Modal";
 
 describe("Modal Component", () => {
   test("ne s'affiche pas quand isOpen est false", () => {
